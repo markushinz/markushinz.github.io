8b0afb2a88aa96988bb0993b614858984cdb333f
Markus Hinz <info@markushinz.de>
2024-09-29 13:57:48 +0200

    deploy

---
+++ api.js
@@ -0,0 +1,4 @@
+if (typeof api == 'undefined') {
+    document.write('<link rel="stylesheet" type="text/css" href="stylesheet.css">')
+    document.write('<script defer src="script.js"></script>')
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+const c = document.getElementsByClassName('container')[0]
+const e = atob('aW5mb0BtYXJrdXNoaW56LmRl')
+c.innerHTML = `<h1>Markus Hinz</h1><a href="mailto:${e}" target="_blank">${e}</a>`
